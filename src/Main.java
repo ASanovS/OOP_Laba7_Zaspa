29cd4e535f358d6320a61859d112c42e512b4a27
ASanovS <serge.asanov@gmail.com>
2018-09-25 12:56:35 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ZPZ-164 Asanov S.V. " + new Date());
 
-        Button btn = new Button("2","1","3","4") {
-        };
+        Button btn = new Button("2","1","3","4");
+        KeyboardButton keyboardButton = new KeyboardButton("1","2","3","5");
 
         System.out.println(btn);
+        System.out.println(keyboardButton);
     }
 }
