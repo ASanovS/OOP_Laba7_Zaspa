9d76805eedc5e89d4168f820b3e95346b7284887
ASanovS <serge.asanov@gmail.com>
2018-09-26 14:23:56 +0300

    reload Interface

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ZPZ-164 Asanov S.V. " + new Date());
 
-        Button btn = new Button("2","1","3","4");
-        KeyboardButton keyboardButton = new KeyboardButton("1","2","3","5");
+        Interface mainInterface = new Interface();
+        mainInterface.interfaceGUI();
 
-        System.out.println(btn);
-        System.out.println(keyboardButton);
     }
 }
